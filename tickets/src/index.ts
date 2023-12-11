1097d65e988e79eec515f5605997ac746f848465
Vjola Shtishi <vjola@Vjolas-MacBook-Pro.local>
2023-12-11 09:14:19 +0100

    added tickets service

---
+++ tickets/src/index.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+const start = async () => {
+    try {
+        if(!process.env.JWT_KEY) {
+            throw new Error('JWT_KEY must be defined');
+        }
+
+        if(!process.env.MONGO_URI) {
+            throw new Error('MONGO_URI must be defined');
+        }
+
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB!!');
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!!')
+});
+
+start();
