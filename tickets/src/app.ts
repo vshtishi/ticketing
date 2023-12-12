9250fbb6f5ada57a730f731727b4c4617c719610
Vjola Shtishi <vjola@adn16.icc-al.org>
2023-12-12 09:11:27 +0100

    unit tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from "cookie-session";
+import { createTicketRouter} from "./routes/new";
+import { currentUser } from "@vshtickets/common";
 
 import { errorHandler, NotFoundError } from "@vshtickets/common";
 
@@ -12,6 +14,9 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }));
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,10 +2,7 @@ import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import {app} from '../app';
 import request from 'supertest';
-
-declare global {
-    var signin: () => Promise<string[]>;
-}
+import jwt from 'jsonwebtoken';
 
 let mongo: any;
 beforeAll(async () => {
@@ -16,13 +13,6 @@ beforeAll(async () => {
     await mongoose.connect(mongoUri, {});
 });
 
-beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections();
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
-});
-
 afterAll(async () => {
     if (mongo) {
         await mongo.stop();
@@ -30,14 +20,20 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-global.signin = async () => {
-    const email = 'test@test.com';
-    const password = 'password';
+declare global {
+    var signin: () => string[];
+}
 
-    const response = await request(app).post('/api/users/signup').send({
-        email, password
-    }).expect(201);
+global.signin = () => {
+    const payload = {
+        id: 'asdfasdf',
+        email: 'test@test.com'
+    };
 
-    return response.get('Set-Cookie');
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+    const session = { jwt: token };
+    const sessionJSON = JSON.stringify(session);
+    const base64 = Buffer.from(sessionJSON).toString('base64');
 
+    return [`session=${base64}`];
 };
