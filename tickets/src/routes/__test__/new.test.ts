964feae8c180aeecbf1aacbb09594ab60866a28c
Vjola Shtishi <vjola@Vjolas-MacBook-Pro.local>
2023-12-13 09:12:07 +0100

    additional tests and middleware fix

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { app } from "../../app";
+import {app} from "../../app";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
     const response = await request(app)
@@ -24,13 +24,46 @@ it("returns a status other than 401 if the user is signed in", async () => {
         .send({});
 
     expect(response.status).not.toEqual(401);
-}   );
+});
 
 it("returns an error if an invalid title is provided", async () => {
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin())
+        .send({
+            title: "",
+            price: 10
+        })
+        .expect(400);
+
+
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin())
+        .send({
+            price: 10
+        })
+        .expect(400);
 });
 
 it("returns an error if an invalid price is provided", async () => {
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin())
+        .send({
+            title: "Test title",
+            price: -10
+        })
+        .expect(400);
 
+
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", global.signin())
+        .send({
+            title: "Test title"
+        })
+        .expect(400);
 });
 
 it("creates a ticket with valid inputs", async () => {
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,20 @@
-import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@vshtickets/common';
+import {Request, Response, NextFunction} from 'express';
+
+const express = require('express');
+import {requireAuth, validateRequest} from '@vshtickets/common';
+import {body} from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-    res.sendStatus(200);
-});
+router.post(
+    '/api/tickets',
+    requireAuth,
+    [body('title').not().isEmpty().withMessage('Title is required'),
+        body('price').isFloat({gt: 0}).withMessage('Price must be provided and must be greater than 0')],
+    validateRequest,
+    (req: Request, res: Response, next: NextFunction) => {
+        res.sendStatus(200);
+    }
+);
 
-export { router as createTicketRouter };
+export {router as createTicketRouter};
