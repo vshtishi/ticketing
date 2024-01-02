0ee3ac42742e153d15a40d15c276fd5f30de67fa
Vjola <vjolashtishi@gmail.com>
2024-01-02 10:42:51 +0100

    tests to fetch ticket by id

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest';
+import { app } from '../../app'
+
+it('returns a 404 if the ticket is not found', async () => {
+    await request(app)
+        .get('/api/tickets/sdfdsf')
+        .send()
+        .expect(404);
+})
+
+it('returns the ticket if the ticket is found', async () => {
+    const title = 'Test ticket'
+    const price = 20;
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title,
+            price
+        })
+        .expect(201);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+        .expect(200)
+})
