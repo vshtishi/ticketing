7c4ad9f25c63eca6c5887dae73a8e3518330e2a9
Vjola Shtishi <vjolashtishi@gmail.com>
2024-01-07 13:27:28 +0100

    update ticket additional tests

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { app } from '../../app';
+import {app} from '../../app';
 import mongoose from 'mongoose';
 
 
@@ -17,17 +17,84 @@ it('returns a 404 if the provided id does not exist', async () => {
 });
 
 it('returns a 401 if the user is not authenticated', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
 
+    await request(app)
+        .put(`/api/tickets/${id}`)
+        .send({
+            title: 'sdds',
+            price: 20
+        })
+        .expect(401);
 });
 
 it('returns a 401 if the user does not own the ticket', async () => {
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'sdds',
+            price: 20
+        });
 
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', global.signin())
+        .send({
+            title: 'sdds',
+            price: 20
+        })
+        .expect(401);
 });
 
 it('returns a 400 if the user provides an invalid title or price', async () => {
+    const cookie = global.signin();
+    const response = await request(app)
 
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'sdds',
+            price: 20
+        });
+
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: 20
+        })
+        .expect(400);
 });
 
 it('returns a 200 if the user provides valid data', async () => {
+    const cookie = global.signin();
+    const response = await request(app)
 
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'sdds',
+            price: 20
+        });
+
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new title',
+            price: 100
+        })
+        .expect(200);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send();
+
+    expect(ticketResponse.body.title).toEqual('new title');
+    expect(ticketResponse.body.price).toEqual(100);
 });
