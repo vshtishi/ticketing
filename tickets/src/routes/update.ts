4d6938ef31d6940c1a8ec6f9e9e3336d55013efc
Vjola Shtishi <vjolashtishi@gmail.com>
2024-01-08 17:25:57 +0100

    update ticket endpoint

---
+++ tickets/src/routes/update.ts
@@ -0,0 +1,37 @@
+import {Request, Response, NextFunction} from 'express';
+
+const express = require('express');
+import {requireAuth, validateRequest} from '@vshtickets/common';
+import {body} from "express-validator";
+import {Ticket} from "../models/ticket";
+
+const router = express.Router();
+
+router.put(
+    '/api/tickets/:id',
+    requireAuth,
+    [body('title').not().isEmpty().withMessage('Title is required'),
+        body('price').isFloat({gt: 0}).withMessage('Price must be provided and must be greater than 0')],
+    validateRequest,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const ticket = await Ticket.findById(req.params.id);
+
+        if (!ticket) {
+            return next(new Error('Ticket not found'));
+        }
+
+        if (ticket.userId !== req.currentUser!.id) {
+            return next(new Error('Not authorized'));
+        }
+
+        ticket.set({
+            title: req.body.title,
+            price: req.body.price
+        });
+
+        await ticket.save();
+        res.send(ticket);
+    });
+
+
+export {router as updateTicketRouter};
